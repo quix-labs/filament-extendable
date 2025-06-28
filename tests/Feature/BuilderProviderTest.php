15effa69c71310358d6e28e2fdd38ee9d91b4515
COLANT Alan <alan.colant@outlook.fr>
2025-06-28 22:04:54 +0200

    [200~test: add tests to verify correct execution order of modifiers

--- tests/Feature/BuilderProviderTest.php
+++ tests/Feature/BuilderProviderTest.php
@@ -24,6 +24,26 @@ test('Ensure schema can be extended at runtime', function () {
     expect($keys)->toBe(['name', 'password']);
 });
 
+test('Ensure schema modifiers are executed in order', function () {
+    static $identifier = "test-schema-order";
+
+    SchemaBuilder::modifySchemaUsing($identifier, function (SchemaBuilder $schemaBuilder) {
+        $schemaBuilder->pushComponents([TextInput::make('password_20')]);
+    }, 20);
+
+    SchemaBuilder::modifySchemaUsing($identifier, function (SchemaBuilder $schemaBuilder) {
+        $schemaBuilder->pushComponents([TextInput::make('password_10')]);
+    }, 10);
+
+    $schema = SchemaBuilder::process(
+        schema: Schema::make(SchemaComponent::make())->components([TextInput::make('name')]),
+        identifier: $identifier
+    );
+
+    $keys = array_keys($schema->getFlatComponents());
+    expect($keys)->toBe(['name', 'password_10', 'password_20']);
+});
+
 test('Ensure table can be extended at runtime', function () {
     static $identifier = "test-table";
     TableBuilder::modifyTableUsing($identifier, function (TableBuilder $tableBuilder) {
@@ -34,7 +54,25 @@ test('Ensure table can be extended at runtime', function () {
         table: Table::make(TableComponent::make())->columns([TextColumn::make('name')]),
         identifier: $identifier
     );
-    
+
     $keys = array_keys($table->getColumns());
     expect($keys)->toBe(['name', 'password']);
+});
+
+test('Ensure table modifiers are executed in order', function () {
+    static $identifier = "test-table-order";
+    TableBuilder::modifyTableUsing($identifier, function (TableBuilder $tableBuilder) {
+        $tableBuilder->pushColumns([TextColumn::make('password_20')]);
+    }, 20);
+    TableBuilder::modifyTableUsing($identifier, function (TableBuilder $tableBuilder) {
+        $tableBuilder->pushColumns([TextColumn::make('password_10')]);
+    }, 10);
+
+    $table = TableBuilder::process(
+        table: Table::make(TableComponent::make())->columns([TextColumn::make('name')]),
+        identifier: $identifier
+    );
+
+    $keys = array_keys($table->getColumns());
+    expect($keys)->toBe(['name', 'password_10', 'password_20']);
 });
