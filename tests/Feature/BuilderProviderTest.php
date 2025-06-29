5767861c95ad16003d75c097c877b0db3aa0d645
COLANT Alan <alan.colant@outlook.fr>
2025-06-29 18:14:10 +0200

    chore(refacto): remove usage of deprecated method

--- tests/Feature/BuilderProviderTest.php
+++ tests/Feature/BuilderProviderTest.php
@@ -8,16 +8,17 @@ use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use QuixLabs\FilamentExtendable\Builders\SchemaBuilder;
 use QuixLabs\FilamentExtendable\Builders\TableBuilder;
+use QuixLabs\FilamentExtendable\Facades\FilamentExtendable;
 use QuixLabs\FilamentExtendable\Tests\Fixtures\SchemaComponent;
 use QuixLabs\FilamentExtendable\Tests\Fixtures\TableComponent;
 
 test('Ensure schema can be extended at runtime', function (): void {
     static $identifier = "test-schema";
-    SchemaBuilder::modifySchemaUsing($identifier, function (SchemaBuilder $schemaBuilder): void {
+    FilamentExtendable::addSchemaModifier($identifier, function (SchemaBuilder $schemaBuilder): void {
         $schemaBuilder->pushComponents([TextInput::make('password')]);
     });
 
-    $schema = SchemaBuilder::process(
+    $schema = FilamentExtendable::processSchema(
         schema: Schema::make(SchemaComponent::make())->components([TextInput::make('name')]),
         identifier: $identifier
     );
@@ -29,15 +30,15 @@ test('Ensure schema can be extended at runtime', function (): void {
 test('Ensure schema modifiers are executed in order', function (): void {
     static $identifier = "test-schema-order";
 
-    SchemaBuilder::modifySchemaUsing($identifier, function (SchemaBuilder $schemaBuilder): void {
+    FilamentExtendable::addSchemaModifier($identifier, function (SchemaBuilder $schemaBuilder): void {
         $schemaBuilder->pushComponents([TextInput::make('password_20')]);
     }, 20);
 
-    SchemaBuilder::modifySchemaUsing($identifier, function (SchemaBuilder $schemaBuilder): void {
+    FilamentExtendable::addSchemaModifier($identifier, function (SchemaBuilder $schemaBuilder): void {
         $schemaBuilder->pushComponents([TextInput::make('password_10')]);
     }, 10);
 
-    $schema = SchemaBuilder::process(
+    $schema = FilamentExtendable::processSchema(
         schema: Schema::make(SchemaComponent::make())->components([TextInput::make('name')]),
         identifier: $identifier
     );
@@ -48,11 +49,11 @@ test('Ensure schema modifiers are executed in order', function (): void {
 
 test('Ensure schema modifiers are not stacked out of context', function (int $iter): void {
     static $identifier = "test-schema-stack";
-    SchemaBuilder::modifySchemaUsing($identifier, function (SchemaBuilder $schemaBuilder) use ($iter): void {
+    FilamentExtendable::addSchemaModifier($identifier, function (SchemaBuilder $schemaBuilder) use ($iter): void {
         $schemaBuilder->pushComponents([TextInput::make("password_iter_{$iter}")]);
     }, $iter);
 
-    $schema = SchemaBuilder::process(schema: Schema::make(SchemaComponent::make()), identifier: $identifier);
+    $schema = FilamentExtendable::processSchema(schema: Schema::make(SchemaComponent::make()), identifier: $identifier);
 
     $keys = array_keys($schema->getFlatComponents());
     expect($keys)->toBe(["password_iter_{$iter}"]);
@@ -60,11 +61,11 @@ test('Ensure schema modifiers are not stacked out of context', function (int $it
 
 test('Ensure table can be extended at runtime', function (): void {
     static $identifier = "test-table";
-    TableBuilder::modifyTableUsing($identifier, function (TableBuilder $tableBuilder): void {
+    FilamentExtendable::addTableModifier($identifier, function (TableBuilder $tableBuilder): void {
         $tableBuilder->pushColumns([TextColumn::make('password')]);
     });
 
-    $table = TableBuilder::process(
+    $table = FilamentExtendable::processTable(
         table: Table::make(TableComponent::make())->columns([TextColumn::make('name')]),
         identifier: $identifier
     );
@@ -75,14 +76,14 @@ test('Ensure table can be extended at runtime', function (): void {
 
 test('Ensure table modifiers are executed in order', function (): void {
     static $identifier = "test-table";
-    TableBuilder::modifyTableUsing($identifier, function (TableBuilder $tableBuilder): void {
+    FilamentExtendable::addTableModifier($identifier, function (TableBuilder $tableBuilder): void {
         $tableBuilder->pushColumns([TextColumn::make('password_20')]);
     }, 20);
-    TableBuilder::modifyTableUsing($identifier, function (TableBuilder $tableBuilder): void {
+    FilamentExtendable::addTableModifier($identifier, function (TableBuilder $tableBuilder): void {
         $tableBuilder->pushColumns([TextColumn::make('password_10')]);
     }, 10);
 
-    $table = TableBuilder::process(
+    $table = FilamentExtendable::processTable(
         table: Table::make(TableComponent::make())->columns([TextColumn::make('name')]),
         identifier: $identifier
     );
@@ -93,11 +94,11 @@ test('Ensure table modifiers are executed in order', function (): void {
 
 test('Ensure table modifiers are not stacked out of context', function (int $iter): void {
     static $identifier = "test-table-stack";
-    TableBuilder::modifyTableUsing($identifier, function (TableBuilder $tableBuilder) use ($iter): void {
+    FilamentExtendable::addTableModifier($identifier, function (TableBuilder $tableBuilder) use ($iter) {
         $tableBuilder->pushColumns([TextColumn::make("password_iter_{$iter}")]);
     }, $iter);
 
-    $table = TableBuilder::process(table: Table::make(TableComponent::make()), identifier: $identifier);
+    $table = FilamentExtendable::processTable(table: Table::make(TableComponent::make()), identifier: $identifier);
 
     $keys = array_keys($table->getColumns());
     expect($keys)->toBe(["password_iter_{$iter}"]);
