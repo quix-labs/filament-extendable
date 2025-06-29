86a5209f9c4e5faeefcc2802615aa49f2cfb4e28
COLANT Alan <alan.colant@outlook.fr>
2025-06-29 17:21:51 +0200

    fix: minimum version 4.0.0-beta5 (getTableGroup) introduction

--- src/Builders/TableBuilder.php
+++ src/Builders/TableBuilder.php
@@ -70,7 +70,7 @@ class TableBuilder
         }
 
         // Retrieve the target group component
-        $group = $this->table->getColumnGroups()[$targetGroup] ?? null;
+        $group = $this->table->getColumnGroup($targetGroup);
         if (!$group instanceof ColumnGroup) {
             throw new TableGroupNotFoundException($targetGroup);
         }
